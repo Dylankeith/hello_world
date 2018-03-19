94492607489daa8682bbc014bb024f2c061b23da
Dylankeith <36173274+Dylankeith@users.noreply.github.com>
2018-03-19 09:53:41 -0400

    Sylvester Input
    Couple lines of code that prompt the user to enter their name then reasds back their name as well as mine

--- hello.java
+++ hello.java
@@ -1,18 +1,33 @@
 // a simple program to practice common Github commands 
 
+import java.util.Scanner;
+String s;
+Scannrt scnr = new scanner (system.in);
+s.scnr.next.();
+
 public class hello {
 
     public static void main(String [] args) {
 
 
+	String f_name;
+	System.out.print("Enter your first name: ");
+	System.out.println("Hello " + f_name + ", my name is 	Dylan!");
+
+	f_name = user_input.next( );
 	
+
+
 	// added  line	
 	// added another line
-
+	// add one more line
         System.out.println("Hello Class!");
+	
+
 
 
 
     }
 }
+
 
